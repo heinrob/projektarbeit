c3aec9a2cab3c70c82fe923d2092c22d285c8a8f
petrosh <petrosh@petrosh.de>
2020-07-27 17:37:47 +0200

    better printing

--- sniff.py
+++ sniff.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from bluepy.btle import Scanner, DefaultDelegate
-from time import time_ns
+import datetime
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -10,13 +10,28 @@ class ScanDelegate(DefaultDelegate):
         self.log = []
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
-        self.log.append((time_ns(), dev))
+        self.log.append((datetime.datetime.timestamp(), dev))
         if isNewDev:
             self.database.append(dev.addr)
         # if isNewDev:
         #     print(f"New device {dev.addr}")
         # elif isNewData:
         #     print(f"New data from {dev.addr}: {dev}")
+
+    def print(self, time, dev):
+        time = datetime.datetime.fromtimestamp(time).strftime("%Y-%m-%d %H:%M:%S.%f")
+        print(f"[ {time} ] - {dev.addr} ({dev.rssi})")
+        for (_, desc, value) in dev.getScanData():
+            print(f"  {desc} = {value}")
+
+    def print_log(self):
+        existing = []
+        for entry in self.log:
+            if entry[1].addr not in existing:
+                existing.append(entry[1].addr)
+                print(f"[ NEW ] - {entry[1].addr}")
+            self.print(*entry)
+            
 
 
 
